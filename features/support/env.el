3f86d2324594f357e7337c644f322e92800ac9fb
Magnar Sveen <magnars@gmail.com>
2013-02-08 20:41:12 +0100

    Setup test framework

---
+++ features/support/env.el
@@ -0,0 +1,37 @@
+;; This is an example of how you could set up this file. This setup
+;; requires a directory called util in the project root and that the
+;; util directory contains the testing tools ert and espuds.
+
+(let* ((features-directory
+        (file-name-directory
+         (directory-file-name (file-name-directory load-file-name))))
+       (project-directory
+        (file-name-directory
+         (directory-file-name features-directory))))
+  (setq tagedit-root-path project-directory)
+  (setq tagedit-util-path (expand-file-name "util" tagedit-root-path)))
+
+(add-to-list 'load-path tagedit-root-path)
+(add-to-list 'load-path (expand-file-name "espuds" tagedit-util-path))
+(add-to-list 'load-path (expand-file-name "ert" tagedit-util-path))
+
+(require 'tagedit)
+(require 'espuds)
+(require 'ert)
+
+
+(Setup
+ ;; Before anything has run
+ )
+
+(Before
+ ;; Before each scenario is run
+ )
+
+(After
+ ;; After each scenario is run
+ )
+
+(Teardown
+ ;; After when everything has been run
+ )
